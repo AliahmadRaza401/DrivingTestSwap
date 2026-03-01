0ad011a3a0f6b9184872fe77367ea01a802973fc
Ali Raza <aliahmadraza401@gmail.com>
2026-03-01 23:06:57 +0500

    icon update

--- lib/core/constants/stripe_key_example.dart
+++
@@ -1,5 +0,0 @@
-// Copy this file to stripe_key.dart and replace with your Stripe keys.
-// Never commit stripe_key.dart (it is in .gitignore).
-
-const String publishableKey = "pk_test_your_publishable_key_here";
-const String secretKey = "sk_test_your_secret_key_here";
